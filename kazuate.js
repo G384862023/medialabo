8d483f07e6e5b18bf8dec45739f8003b140620fd
G384862023 <238486@st.takushoku-u.ac.jp>
2024-08-06 22:04:21 +0900

    更新

--- kazuate.js
+++ kazuate.js
@@ -8,26 +8,34 @@ let kaisu = 0;
 // 予想を4回実行する
 // 将来以下の hantei(); の4回の呼び出しを全て削除する
 // 代わりにここでは，ボタンを押したら hantei() を呼び出すイベント処理をする
-hantei();
+let b = document.querySelector('#print');
+b.addEventListener('click', greeting);
+function greeting() {
+  let i = document.querySelector('input[name="kazu"]');
+  let yoso = parseInt(i.value,10);
+  hantei();
+}
+
 
 // ボタンを押した後の処理をする関数 hantei() の定義
 function hantei() {
   // 将来ここでは 4 ではなくテキストボックスに指定された数値を yoso に代入する
-  let yoso = 4;
+  i = document.querySelector('input[name="kazu"]');
+  yoso = parseInt(i.value,10);
+  
   kaisu=kaisu+1;
   let k1=document.querySelector('span#kaisu');
   k1.textContent = kaisu;
 
   let k2=document.querySelector('span#answer');
   k2.textContent = yoso;
-
+  let e=0;
   let p=document.querySelector('p#result');
-  if(kaisu<=4){
-    //console.log(kaisu+"回目の予想: "+yoso);
+  if(kaisu<4&&e===0){
       // 課題3-1: 正解判定する
     if(yoso===kotae){
       p.textContent ='正解です.おめでとう!';
-        kaisu=kaisu+4;
+      e=1;
       }
       else if(yoso>kotae&&kaisu<=2){
           p.textContent ="まちがえ.答えはもっと小さいです";
--- weather.js
+++ weather.js
@@ -1,60 +1,73 @@
-let data = {
-  "coord": {
-    "lon": 116.3972,
-    "lat": 39.9075
-  },
-  "weather": [
-    {
-      "id": 803,
-      "main": "Clouds",
-      "description": "曇りがち",
-      "icon": "04d"
-    }
-  ],
-  "base": "stations",
-  "main": {
-    "temp": 9.94,
-    "feels_like": 8.65,
-    "temp_min": 9.94,
-    "temp_max": 9.94,
-    "pressure": 1022,
-    "humidity": 14,
-    "sea_level": 1022,
-    "grnd_level": 1016
-  },
-  "visibility": 10000,
-  "wind": {
-    "speed": 2.65,
-    "deg": 197,
-    "gust": 4.84
-  },
-  "clouds": {
-    "all": 53
-  },
-  "dt": 1646542386,
-  "sys": {
-    "type": 1,
-    "id": 9609,
-    "country": "CN",
-    "sunrise": 1646520066,
-    "sunset": 1646561447
-  },
-  "timezone": 28800,
-  "id": 1816670,
-  "name": "北京市",
-  "cod": 200
-};
+function print(data) {
+var resultDiv = document.getElementById("result");
+  resultDiv.textContent = ""; // Clear previous results
 
-////////// 課題3-2 ここからプログラムを書こう
-let div = document.querySelector('div#result'); 
-let w=document.createElement('a');
-w.textContent = data.name;
-w.textContent = data.id;
-w.textContent = data.weather[0].description;
-w.textContent = data.main.temp_min;
-w.textContent = data.main.temp_max;
-div.insertAdjacentElement('beforeend', w);
-/*console.log(data.id);
-console.log(data.weather[0].description);
-console.log(data.main.temp_min);
-console.log(data.main.temp_max);*/
+  // 関数を使ってデータを追加
+  function addDataToResult(label, value) {
+    var p = document.createElement("p");
+    p.textContent = label + ": " + value;
+    resultDiv.appendChild(p);
+  }
+
+  addDataToResult("都市名", data.name);
+  addDataToResult("経度", data.coord.lon);
+  addDataToResult("緯度", data.coord.lat);
+  addDataToResult("天気", data.weather[0].description);
+  addDataToResult("最低気温", data.main.temp_min + "°C");
+  addDataToResult("最高気温", data.main.temp_max + "°C");
+
+  console.log(data.coord.lon);
+}
+
+let b = document.querySelector('button#btn');
+b.addEventListener('click', sendRequest);
+
+// 通信を開始する処理
+function sendRequest() {
+  let s = document.querySelector('select#tenki');
+  let idx = s.selectedIndex; // idx 番目の option が選択された
+
+  let os = s.querySelectorAll('option'); // s の子要素 option をすべて検索
+  let o = os.item(idx); // os の idx 番目の要素
+
+  console.log('選択された ' + idx + ' 番目の option の情報:');
+  console.log('  value=' + o.getAttribute('value')); // id 属性を表示
+  console.log('  textContent=' + o.textContent);
+
+  // URL を設定
+  let url = 'https://www.nishita-lab.org/web-contents/jsons/openweather/' + o.getAttribute('value') + '.json';
+
+  // 通信開始
+  axios.get(url)
+    .then(showResult) // 通信成功
+    .catch(showError) // 通信失敗
+    .then(finish); // 通信の最後の処理
+}
+
+// 通信が成功した時の処理
+function showResult(resp) {
+  // サーバから送られてきたデータを出力
+  let data = resp.data;
+
+  // data が文字列型なら，オブジェクトに変換する
+  if (typeof data === 'string') {
+    data = JSON.parse(data);
+  }
+  print(data);
+  // data をコンソールに出力
+  console.log(data);
+
+  // data.x を出力
+  console.log(data.x);
+  console.log(data.coord.lon);
+}
+
+// 通信エラーが発生した時の処理
+function showError(err) {
+  console.log(err);
+}
+
+// 通信の最後にいつも実行する処理
+function finish() {
+  console.log('Ajax 通信が終わりました');
+}
